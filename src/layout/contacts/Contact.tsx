4840b0a7c27df7a88e54ae91b476fbf4ba098be4
Aisultan Abdykerov <abdykerovaisultan@gmail.com>
2023-11-02 16:22:38 +0600

    Refactoring Contact And Testimony

--- src/layout/contacts/Contact.tsx
+++ src/layout/contacts/Contact.tsx
@@ -1,67 +1,23 @@
 import React from 'react';
-import styled from "styled-components";
 import {SectionTitle} from "../../components/section-title/SectionTitle";
 import {Button} from "../../components/button/Button";
 import {Container} from "../../components/Container/Container";
-import {theme} from "../../styles/Theme";
+import {S} from './Contact_Styles'
 
-export const Contact = () => {
+export const Contact:React.FC = () => {
     return (
-        <StyledContact>
+        <S.Contact>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <StyledForm>
-                    <Field placeholder={'name'}/>
-                    <Field placeholder={'subject'}/>
-                    <Field placeholder={'message'} as={'textarea'}/>
+                <S.Form>
+                    <S.Field placeholder={'name'}/>
+                    <S.Field placeholder={'subject'}/>
+                    <S.Field placeholder={'message'} as={'textarea'}/>
                     <Button type={'submit'}>Send Message</Button>
-                </StyledForm>
+                </S.Form>
             </Container>
-        </StyledContact>
+        </S.Contact>
     );
 };
-
-
-const StyledContact = styled.section`
-    
-`
-
-const StyledForm = styled.form`
-  max-width: 540px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 16px;  
-  margin: 0 auto;
-  
-  textarea{
-    resize: none;
-    height: 155px;
-  }
-`
-
-const Field = styled.input`
-  width: 100%;
-  background-color: ${theme.colors.secondaryBg};
-  border: 1px solid ${theme.colors.borderColor};
-  padding: 7px 15px;
-  
-  font-family: 'Poppins', sans-serif;
-  font-weight: 400;
-  font-size: 12px;
-  letter-spacing: 0.05em;
-  
-  color: ${theme.colors.font};
-  
-  &::placeholder{
-    color: ${theme.colors.placeholderColor};
-    text-transform: capitalize;
-  }
-  
-  &:focus-visible{
-    outline: 1px solid ${theme.colors.borderColor};
-  }
-`
 
 
